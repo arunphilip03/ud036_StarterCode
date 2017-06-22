40f314d972b38eb2dce9eb603b2de4fb865206d6
arunphilip03 <philip03@gmail.com>
2017-06-22 16:47:44 +0530

    formatted code

--- media.py
+++ media.py
@@ -1,11 +1,13 @@
 import webbrowser
 
+
 class Movie(object):
     """ This class stores movie related information.
     Attributes:
         title: A string indicating the title of the movie.
         poster_image_url: A string to hold the poster image url of the movie.
-        trailer_youtube_url: A string to hold the youtube trailer url of the movie.
+        trailer_youtube_url: A string to hold the youtube trailer url
+                             of the movie.
     """
 
     def __init__(self, title, poster_image_url, trailer_youtube_url):
@@ -13,5 +15,3 @@ class Movie(object):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-        
-        
